6deb64b4dadbc5679be6788f416e07bd98455054
yzs <zsyang168@163.com>
2018-12-30 08:48:27 +0800

    add product

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -2,3 +2,21 @@
 layui.use('element', function(){
     var element = layui.element;
 });
+
+layui.use('carousel', function(){
+    var carousel = layui.carousel;
+    //建造实例
+    carousel.render({
+      elem: '#test1'
+      ,width: '100%' //设置容器宽度
+      ,arrow: 'hover' //始终显示箭头
+      ,height: '500px'
+      //,anim: 'updown' //切换动画方式
+    });
+    //监听轮播切换事件
+    carousel.on('change(test1)', function(obj){ //test1来源于对应HTML容器的 lay-filter="test1" 属性值
+    console.log(obj.index); //当前条目的索引
+    console.log(obj.prevIndex); //上一个条目的索引
+    console.log(obj.item); //当前条目的元素对象
+    });   
+});
